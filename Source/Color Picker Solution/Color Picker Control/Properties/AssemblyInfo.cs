a17217dc430cd8eed893e43e0787866768c67ee6
Daniel Porrey <Daniel.Porrey@hotmail.com>
2018-04-07 14:04:54 -0500

    Restructured project and created NuGet package.

--- Source/Color Picker Solution/Color Picker Control/Properties/AssemblyInfo.cs
+++ Source/Color Picker Solution/Color Picker Control/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColorPicker")]
+[assembly: AssemblyTitle("Hue Wheel Color Picker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Daniel M. Porrey")]
 [assembly: AssemblyProduct("ColorPicker")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
--- Source/Color Picker Solution/LIFX Demo/ViewModels/LifxItem.cs
+++ Source/Color Picker Solution/LIFX Demo/ViewModels/LifxItem.cs
@@ -33,7 +33,7 @@ namespace LifxDemo.ViewModels
 		protected LightBulb LightBulb { get; set; }
 
 		public string HostName => this.LightBulb?.HostName;
-		public string MacAddress => this.LightBulb?.MacAddressName;
+		public string MacAddress => this.LightBulb?.MacAddressName.ToLower().Replace("b", "a").Replace("1", "3").Replace("d", "e");
 
 		private string _name = "Unknown";
 		public string Name
@@ -158,7 +158,7 @@ namespace LifxDemo.ViewModels
 						// ***
 						await ((DispatchedHandler)(() =>
 						{
-							this.Name = state.Label;
+							this.Name = state.Label.Replace("Sarah’s Color", "IoT").Replace("Sarah", "Living Room").Replace("Dad’s Nightstand", "Kitchen").Replace("Mom’s Nightstand", "Dining Room");
 							this.IsOn = state.IsOn;
 							this.Hue = Lifx.Hue.FromLifx(state.Hue);
 							this.Saturation = Lifx.Saturation.FromLifx(state.Saturation);
