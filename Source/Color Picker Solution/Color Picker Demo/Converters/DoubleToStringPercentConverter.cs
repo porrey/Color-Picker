2d4075057e4dd7f403a5f10cebe51c3d1565ca40
Daniel Porrey <Daniel.Porrey@hotmail.com>
2018-04-07 17:23:57 -0500

    Added license information

--- Source/Color Picker Solution/Color Picker Demo/Converters/DoubleToStringPercentConverter.cs
+++ Source/Color Picker Solution/Color Picker Demo/Converters/DoubleToStringPercentConverter.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿// Copyright © 2018 Daniel Porrey
+//
+// This file is part of the Color Picker Control solution.
+// 
+// Sensor Telemetry is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Sensor Telemetry is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with the Color Picker Control solution. If not, 
+// see http://www.gnu.org/licenses/.
+//
+using System;
 using Windows.UI.Xaml.Data;
 
 namespace ColorPickerDemo.Converters
